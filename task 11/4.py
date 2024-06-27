3b93c4f3095a430f7bab152e0f98972949de144e
Maria-Shakhverdyan <147909756+Maria-Shakhverdyan@users.noreply.github.com>
2024-06-27 16:00:46 +0400

    Update 4.py

--- task 11/4.py
+++ task 11/4.py
@@ -5,3 +5,12 @@ for char in reverse:
     new_string = char + new_string
 
 print(new_string)
+
+
+# reverse = "reverse me"
+# new_string = ""
+
+# for char in reverse:
+#     new_string = reverse[::-1]
+
+# print(new_string)
