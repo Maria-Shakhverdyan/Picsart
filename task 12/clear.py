8d6d4f7212382c2f0f4f03cbed0e5b590ef54d52
Maria-Shakhverdyan <147909756+Maria-Shakhverdyan@users.noreply.github.com>
2024-07-01 16:18:53 +0400

    Update clear.py

--- task 12/clear.py
+++ task 12/clear.py
@@ -1,15 +1,18 @@
 ls = []
-n = int(input("Enter the list: "))
+n = int(input("Enter the list size: "))  # Поправил текст запроса на ввод
+
 count = 0
 
 while count < n:
-    element = (int(input("Enter the element: ")))
+    element = int(input("Enter the element: "))
     ls.append(element)
     count += 1
 
-if len(ls) == 0:
-    raise IndexError("Clear from empty list")
-
-ls[:] = []
-
-print(ls)
+try:
+    if len(ls) == 0:
+        raise IndexError("Clear from empty list")
+    
+    ls[:] = []
+    print(ls)
+except IndexError as e:
+    print(f"An error occurred: {e}")
