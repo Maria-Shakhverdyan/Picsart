093e3d1f68a97ead0a7d1f9fe0e3429b95c26873
Maria-Shakhverdyan <147909756+Maria-Shakhverdyan@users.noreply.github.com>
2024-07-07 18:19:39 +0400

    Update user_data.py

--- task 15/user_data.py
+++ task 15/user_data.py
@@ -1,7 +1,19 @@
+import random
+
 users_dict = {}
 users_list = []
 
-def add_user(user_id, first_name, last_name, email, password, phone):
+def generate_user_id():
+    return ''.join(random.choices('1234567890', k=6))
+
+def add_user():
+    user_id = generate_user_id()
+    first_name = input("Enter first_name: ")
+    last_name = input("Enter last_name: ")
+    email = input("Enter email: ")
+    password = input("Enter password: ")
+    phone = input("Enter phone: ")
+
     user = {
         'id': user_id,
         'first_name': first_name,
@@ -10,34 +22,25 @@ def add_user(user_id, first_name, last_name, email, password, phone):
         'password': password,
         'phone': phone
     }
+
     users_dict[user_id] = user
     users_list.append(user)
 
-def find_user_by_name(first_name):
-    result = [user for user in users_list if user['first_name'] == first_name]
-    if result:
-        return result
+def find_user_by_name():
+    name_to_search = input("Enter first_name for search: ")
+    found_users = [user for user in users_list if user['first_name'] == name_to_search]
+    if found_users:
+        print("Found users: ", found_users)
     else:
-        return "Not found"
-
-def import_user_data():
-    user_id = input("Enter user_id: ")
-    first_name = input("Enter first_name: ")
-    last_name = input("Enter last_name: ")
-    email = input("Enter email: ")
-    password = input("Enter password: ")
-    phone = input("Enter phone: ")
-    add_user(user_id, first_name, last_name, email, password, phone)
+        print("Not found")
 
 while True:
     action = input("Select action: \n[1] Import user data \n[2] Find user by name \n[3] Exit\n")
     if action == '1':
-        import_user_data()
+        add_user()
     elif action == '2':
-        name_to_search = input("Enter first_name for search: ")
-        found_users = find_user_by_name(name_to_search)
-        print("Found users: ", found_users)
+        find_user_by_name()
     elif action == '3':
         break
     else:
-        print("Incorrect password, try again.")
+        print("Incorrect option, please try again.")
