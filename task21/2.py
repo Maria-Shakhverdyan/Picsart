70e828aaf4273ec16aa0a5bd36a11d49898ead4f
Maria-Shakhverdyan <147909756+Maria-Shakhverdyan@users.noreply.github.com>
2024-07-27 16:21:13 +0400

    Update 2.py

--- task21/2.py
+++ task21/2.py
@@ -1,11 +1,10 @@
-def calculate_total_price(*item_prices, tax_rate = 0.08):
-    subtotal = sum(item_prices)
-    tax = subtotal * tax_rate
-    total_price = subtotal + tax
+def calculate_total_price(item_price, /, tax_rate = 0.08):
+    tax = item_price * tax_rate
+    total_price = item_price + tax
     return total_price
 
-result_1 = calculate_total_price(100, 100)
+result_1 = calculate_total_price(100)
 print("The result_1 is: ", result_1)
 
-result_2 = calculate_total_price(20, 20, tax_rate = 0.2)
+result_2 = calculate_total_price(20, tax_rate = 0.2)
 print("The result_2 is: ", result_2)
