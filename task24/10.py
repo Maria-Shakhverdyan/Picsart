9ce5b7deb4ae458d87f80e2d85a0a9e0c29e325c
Maria-Shakhverdyan <147909756+Maria-Shakhverdyan@users.noreply.github.com>
2024-08-04 17:34:01 +0400

    Update 10.py

--- task24/10.py
+++ task24/10.py
@@ -3,14 +3,6 @@ def make_config(key, value):
         return {key: value}
     return config
 
-# person_info = {
-#     "name": "Alice",
-#     "age": 30,
-#     "city": "New York",
-#     "email": "alice@example.com",
-#     "is_employee": True,
-# }
-
 config_func = make_config("Ann", "31")
 config_dict = config_func()
 
