001da917ecb38d236efb10a4434368697cbe61cc
Maria-Shakhverdyan <147909756+Maria-Shakhverdyan@users.noreply.github.com>
2024-09-26 17:52:58 +0400

    Update conversation.py

--- messageaccount/conversation.py
+++ messageaccount/conversation.py
@@ -1,21 +1,23 @@
-from typing import List
-from user import User
-from message import Message
+from typing import NewType, List
+
+UserType = NewType('UserType', object)
+MessageType = NewType('MessageType', object)
+ConversationType = NewType('ConversationType', object)
 
 class Conversation:
-    def __init__(self, participants: List['User'], message_history: List['Message'] = []) -> None:
+    def __init__(self, participants: List[UserType], message_history: List[MessageType] = []) -> None:
         self.__participants = participants
         self.__message_history = message_history
 
-    def add_message(self, message: 'Message') -> None:
+    def add_message(self, message: MessageType) -> None:
         self.__message_history.append(message)
 
-    def add_user(self, user: 'User') -> None:
+    def add_user(self, user: UserType) -> None:
         if user not in self.__participants:
             self.__participants.append(user)
 
-    def get_messages(self) -> List['Message']:
+    def get_messages(self) -> List[MessageType]:
         return self.__message_history
 
-    def get_participants(self) -> List['User']:
+    def get_participants(self) -> List[UserType]:
         return self.__participants
